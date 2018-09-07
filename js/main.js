586ea967eecbe88c297918bde14951deac0d9714
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-09-06 22:08:25 -0700

    component

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+//create component which we can reuse grobally
+Vue.component('hello', {
+  template: '<h1>Component!</h1>'
+});
+
 var vm1 = new Vue({
-  el: '#app1',
+  // el: '#app1', // write in $mount()
   data: {
     title: 'This VueJS instance',
     showParagraph: false,
@@ -8,6 +13,8 @@ var vm1 = new Vue({
     show: function() {
       this.showParagraph = true;
       this.updateTitle('This VueJS Instance (Updated)');
+      //$refs is like jQuery
+      this.$refs.myButton.innerText = 'Test';
     },
     updateTitle: function(t) {  // t = ('this vueJS instance (updated)')
       this.title = t;
@@ -25,10 +32,17 @@ var vm1 = new Vue({
   }
 });
 
+//$mount is method. It works same if you put el:'#app' but different syntax
+vm1.$mount('#app1')
+
+vm1.$refs.heading.innerText = 'Somthing else';
+
 setTimeout(function(){
   vm1.title = 'Changed by Timer!'
   vm1.show()
 },3000)
+
+
 
 var vm2 = new Vue({
   el: '#app2',
@@ -40,4 +54,15 @@ var vm2 = new Vue({
       vm1.title = 'Changed!';
     }
   }
-})
+});
+
+
+var vm3 = new Vue({
+  //we can create own template
+  template: '<h1>Hello!</h1>',
+});
+
+// vm3.$mount('#app3')
+//or we can write as follows. But the following way is not common
+vm3.$mount()
+document.getElementById('app3').appendChild(vm3.$el)
